6dabd1327dde52f570f26577540703e3ee13413b
Brad Gessler <brad@bradgessler.com>
2010-03-13 18:03:21 -0800

    Fixed the url redirector to include a hyperlink

--- lib/anywhere/redirector.rb
+++ lib/anywhere/redirector.rb
@@ -3,6 +3,6 @@ module Anywhere
     location = Anywhere.configuration.redirect_to
     status   = Anywhere.configuration.redirect_status
     
-    [ status, {"Content-Type" => "text/plain", "Location" => location}, "Redirecting to #{location}...\n" ]
+    [ status, {"Content-Type" => "text/plain", "Location" => location}, %{Redirecting to <a href="#{location}">#{location}</a>...\n} ]
   }
 end
